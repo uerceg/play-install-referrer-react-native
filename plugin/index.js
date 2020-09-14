24426bf349f4753fa9eec482c3e4327ccc2047d2
ugi <ugi@adjust.com>
2020-09-15 01:46:50 +0200

    add new version 1.1.5

--- plugin/index.js
+++ plugin/index.js
@@ -1,6 +1,7 @@
 //
 //  index.js
 //  play-install-referrer-react-native
+//  version: 1.1.5
 //
 //  Created by Uglješa Erceg (@ugi) on 24th April 2020.
 //  Copyright (c) 2020 Uglješa Erceg. All rights reserved.
