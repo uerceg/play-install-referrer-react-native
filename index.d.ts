8afbd152df0f7dc2cfd2ba2fab61d9fbb6dfe902
AP Fritts <ap@apfritts.com>
2021-05-19 14:41:36 -0700

    Additional TypeScript definitions

--- index.d.ts
+++ index.d.ts
@@ -7,7 +7,26 @@
 //
 
 declare module 'react-native-play-install-referrer' {
+  export class PlayInstallReferrerError extends Error {
+    responseCode: string
+  }
+
+  export type PlayInstallReferrerInfo = {
+    googlePlayInstant: boolean
+    installBeginTimestampSeconds: string
+    installBeginTimestampServerSeconds: string
+    installReferrer: string
+    installVersion: string
+    referrerClickTimestampSeconds: string
+    referrerClickTimestampServerSeconds: string
+  }
+
+  export type PlayInstallReferrerCallback = (
+    info: PlayInstallReferrerInfo | null,
+    error: PlayInstallReferrerError | null,
+  ) => void
+
   export const PlayInstallReferrer: {
-    getInstallReferrerInfo: (callback) => void
+    getInstallReferrerInfo: (callback: PlayInstallReferrerCallback) => void
   }
 }
